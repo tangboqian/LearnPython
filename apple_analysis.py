7cb3374b7a774d4491b3d1376b8a54cf0280b8e2
tangboqian <boqianta@gmail.com>
2018-01-07 23:35:01 -0800

    Update apple_analysis.py

--- apple_analysis.py
+++ apple_analysis.py
@@ -4,12 +4,19 @@ from pandas import DataFrame
 def quarter_volume():
 	second_volume = 0
 	data = pd.read_csv('apple.csv', header = 0)
-	# df  = DataFrame(data)
-	i = pd.data_range(data['Date'])
-	dd = pd.Series(data.Volume, index = i)
+	# transfer a rangeIndex instance to a DatetimeIndex instance
+	i = pd.to_datetime(data.index)
+	vol_data = pd.Series(data.Volume, index = i)
 
-	dd = dd.resample('M').sum()
+	vol_data = vol_data.resample('3M').sum()
 	print(dd.head(10))
+	
+	#find the second-max volume
+	maxV = vol_data.max()
+	for v in vol_data.Volume:
+		if v > second_volume && v < max:
+			second_volume = v
+	
 	return second_volume
 
 quarter_volume()
